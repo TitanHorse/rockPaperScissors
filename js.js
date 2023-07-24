fc4f1261d15d97245e901fde553db5126c26d0c1
TitanHorse <storerobert@hotmail.se>
2023-07-24 15:42:09 +0200

    Finish comp and player selection

--- js.js
+++ js.js
@@ -14,6 +14,19 @@ switch(getComputerChoice()) {
         break;
 }
 console.log(getComputerChoice);
-
+let computerSelection = getComputerChoice;
 let userInput = prompt("Choose 'rock', 'paper', or 'scissors'");
+
 let playerSelection = userInput.toLowerCase();
+
+if (playerSelection === computerSelection) {
+    alert("Tie!");
+} else if (playerSelection === "rock") {
+    (computerSelection === "scissors") ? console.log("You win!"): console.log("You lose!");
+} else if (playerSelection === "scissors") {
+    (computerSelection === "paper") ? console.log("You win!") : console.log("You lose!");
+} else if (playerSelection === "paper") {
+    (computerSelection === "rock") ? console.log("You win!") : console.log("You lose!");
+} else {
+    console.log("Choose between rock, paper or scissors");
+}
